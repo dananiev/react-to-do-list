9bfa013393078330bcbe963c0768d2a71375849b
Deyan Ananiev <deyan.ananiev@gmail.com>
2020-09-27 14:38:09 +0300

    Updates

---
+++ precache-manifest.3b9faab039845bb7b28d2c2e0764fcb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfbe1cbb1c9d4ee1db5bde4832425d30",
+    "url": "/react-to-do-list/index.html"
+  },
+  {
+    "revision": "cc53490eda4c34258525",
+    "url": "/react-to-do-list/static/css/main.be16362d.chunk.css"
+  },
+  {
+    "revision": "6b920278d3192cdf5d63",
+    "url": "/react-to-do-list/static/js/2.0d550a27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-to-do-list/static/js/2.0d550a27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc53490eda4c34258525",
+    "url": "/react-to-do-list/static/js/main.60651fb5.chunk.js"
+  },
+  {
+    "revision": "8c3d49c384a4bcf49218",
+    "url": "/react-to-do-list/static/js/runtime-main.f9d0e86f.js"
+  }
+]);
